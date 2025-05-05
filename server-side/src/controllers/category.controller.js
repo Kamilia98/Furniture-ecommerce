fb547bb4079caf517b1b2c66dfc737071eaf5a9e
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-06 00:53:45 +0300

    extends getCategories

--- server-side/src/controllers/category.controller.js
+++ server-side/src/controllers/category.controller.js
@@ -1,20 +1,80 @@
 const Category = require('../models/category.model');
+const Product = require('../models/product.model');
 const httpStatusText = require('../utils/httpStatusText');
 const AppError = require('../utils/appError');
 const asyncWrapper = require('../middlewares/asyncWrapper.middleware');
+const mongoose = require('mongoose');
 
 const getAllCategories = asyncWrapper(async (req, res, next) => {
-  const categories = await Category.find().select('-__v').lean();
-  if (!categories.length) {
+  const categoriesWithProductCount = await Category.aggregate([
+    {
+      $lookup: {
+        from: 'products',
+        localField: '_id',
+        foreignField: 'categories',
+        as: 'products',
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        image: 1,
+        productCount: { $size: '$products' },
+      },
+    },
+  ]);
+
+  if (!categoriesWithProductCount.length) {
     return next(new AppError('No categories found.', 404, httpStatusText.FAIL));
   }
 
   res.status(200).json({
     status: httpStatusText.SUCCESS,
-    data: { categories },
+    data: { categories: categoriesWithProductCount },
+  });
+});
+
+const getCategoryDetails = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError('Invalid category ID.', 400, httpStatusText.FAIL));
+  }
+
+  const existingCategory = await Category.findById(id);
+  if (!existingCategory) {
+    return next(new AppError('Category not found.', 404, httpStatusText.FAIL));
+  }
+
+  const categoryDetails = await Category.aggregate([
+    {
+      $match: { _id: new mongoose.Types.ObjectId(id) },
+    },
+    {
+      $lookup: {
+        from: 'products',
+        localField: '_id',
+        foreignField: 'categories',
+        as: 'products',
+      },
+    },
+    {
+      $project: {
+        name: 1,
+        description: 1,
+        image: 1,
+        products: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { category: categoryDetails[0] },
   });
 });
 
 module.exports = {
   getAllCategories,
+  getCategoryDetails,
 };
