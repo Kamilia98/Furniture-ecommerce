cd872ea9044701418898767c17a731341fe3cecc
Kamilia <90932389+Kamilia98@users.noreply.github.com>
2025-05-06 19:33:06 +0300

    added delete, edit and add category functionalities

--- server-side/src/controllers/category.controller.js
+++ server-side/src/controllers/category.controller.js
@@ -19,6 +19,7 @@ const getAllCategories = asyncWrapper(async (req, res, next) => {
       $project: {
         name: 1,
         image: 1,
+        description: 1,
         productCount: { $size: '$products' },
       },
     },
@@ -74,7 +75,92 @@ const getCategoryDetails = asyncWrapper(async (req, res, next) => {
   });
 });
 
+const editCategory = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params;
+  const { name, description, image } = req.body;
+
+  // Validate the category ID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError('Invalid category ID.', 400, httpStatusText.FAIL));
+  }
+
+  // Find and update the category
+  const updatedCategory = await Category.findByIdAndUpdate(
+    id,
+    { name, description, image },
+    { new: true, runValidators: true } // Return the updated document and validate the input
+  );
+
+  // If the category is not found
+  if (!updatedCategory) {
+    return next(new AppError('Category not found.', 404, httpStatusText.FAIL));
+  }
+
+  // Respond with the updated category
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    data: { category: updatedCategory },
+  });
+});
+
+const addCategory = asyncWrapper(async (req, res, next) => {
+  const { name, description, image } = req.body;
+
+  const existingCategory = await Category.findOne({ name });
+  if (existingCategory) {
+    return next(
+      new AppError(
+        'Category with this name already exists.',
+        400,
+        httpStatusText.FAIL
+      )
+    );
+  }
+
+  // Create a new category
+  const newCategory = await Category.create({ name, description, image });
+
+  // Respond with the newly created category
+  res.status(201).json({
+    status: httpStatusText.SUCCESS,
+    data: { category: newCategory },
+  });
+});
+
+const deleteCategory = asyncWrapper(async (req, res, next) => {
+  const { id } = req.params; // Get the category ID from the request parameters
+  console.log(id);
+  // Validate the category ID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return next(new AppError('Invalid category ID.', 400, httpStatusText.FAIL));
+  }
+
+  // Find and delete the category
+  const deletedCategory = await Category.findByIdAndDelete(id);
+
+  // If the category is not found
+  if (!deletedCategory) {
+    return next(new AppError('Category not found.', 404, httpStatusText.FAIL));
+  }
+
+  // Remove the category reference from all products
+  await Product.updateMany(
+    { categories: id }, // Find products that reference the deleted category
+    { $pull: { categories: id } } // Remove the category ID from the categories array
+  );
+
+  // Respond with a success message
+  res.status(200).json({
+    status: httpStatusText.SUCCESS,
+    message:
+      'Category deleted successfully and references removed from products.',
+  });
+});
+
 module.exports = {
   getAllCategories,
   getCategoryDetails,
+  editCategory,
+  addCategory,
+  deleteCategory,
 };
